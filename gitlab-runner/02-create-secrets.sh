fc14891ae94a2aa40799dfc43c4fc3c68bb94b8e
Alejandro Hidalgo <alejandro.hidalgo@gmail.com>
2021-11-05 19:55:10 -0300

    Update code

--- gitlab-runner/02-create-secrets.sh
+++ gitlab-runner/02-create-secrets.sh
@@ -1,1 +1,5 @@
-kubectl create secret generic gitlab-secret --namespace gitlab --from-file=./REGISTRATION_TOKEN
+kubectl --namespace gitlab create secret generic gitlab-secret \
+  --from-file=./REGISTRATION_TOKEN
+
+kubectl create secret generic google-application-credentials \
+  --from-file=gcs-application-credentials-file=sa/sa-base-images.json
